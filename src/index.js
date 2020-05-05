992fb980def44258a39f4d433a2eadda28033974
Pavel Zhdanovich <zhdan21@gmail.com>
2020-05-05 16:39:24 +0300

    task 1 completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-import './css/style.css';
+/* eslint-disable no-new */
+/* eslint-disable no-prototype-builtins */
+export default function Character(name, type) {
+  const errorName = new Error('Имя должно содержать не менее 2 символов, но не более 10');
+  const errorType = new Error('Такого класса не существует');
+  const characterBase = {
+    Bowman: { attack: 25, defence: 25 },
+    Swordsman: { attack: 40, defence: 10 },
+    Magician: { attack: 10, defence: 40 },
+    Undead: { attack: 25, defence: 25 },
+    Zombie: { attack: 40, defence: 10 },
+    Daemon: { attack: 10, defence: 40 },
+  };
+  if (name.length > 10 || name.length < 2 || (!typeof name === 'string')) {
+    return errorName;
+  }
+  if (!characterBase.hasOwnProperty(type) || (!typeof name === 'string')) {
+    return errorType;
+  }
+  const { attack, defence } = characterBase[type];
 
-import './js/app';
+  this.name = name;
+  this.type = type;
+  this.health = 100;
+  this.level = 1;
+  this.attack = attack;
+  this.defence = defence;
+}
 
-// TODO: write your code in app.js
+new Character('Zn', 'Zombie');
